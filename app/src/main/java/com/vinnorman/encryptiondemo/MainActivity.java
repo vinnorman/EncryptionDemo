7c6b4dc7d4fa6e4770f9f4a9a60b215e90f69fef
Vin Norman <vin.norman@gmail.com>
2018-04-24 11:47:53 +0100

    Add basic layout, view references and click listeners
    Single activity app, with layout as ConstraintLayout and all necessary layout widgets in place. No styling yet. Button click listeners just have basic test functionality.
    
    App is now ready to work on the encryption and decryption logic.

--- app/src/main/java/com/vinnorman/encryptiondemo/MainActivity.java
+++ app/src/main/java/com/vinnorman/encryptiondemo/MainActivity.java
@@ -1,13 +1,54 @@
 package com.vinnorman.encryptiondemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button encryptButton, decryptButton;
+    private TextView encryptedTextView, decryptedTextView;
+    private EditText plainTextInputField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        encryptButton = findViewById(R.id.button_encrypt);
+        decryptButton = findViewById(R.id.button_decrypt);
+        decryptedTextView = findViewById(R.id.text_decrypted);
+        encryptedTextView = findViewById(R.id.text_encrypted);
+        plainTextInputField = findViewById(R.id.edit_plain_text);
+
+        encryptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String plainTextString = plainTextInputField.getText().toString();
+                encryptedTextView.setText(plainTextString);
+            }
+        });
+
+        decryptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String decryptedTextString = encryptedTextView.getText().toString();
+                decryptedTextView.setText(decryptedTextString);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity, menu);
+        return true;
     }
 }
