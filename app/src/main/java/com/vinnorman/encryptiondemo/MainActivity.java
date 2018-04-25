c23dbd438ad01df685a69fc3747bdd9d4f4f2f58
Vin Norman <vin.norman@gmail.com>
2018-04-25 12:07:15 +0100

    Refactor Main Activity, Main Layout
    Seperated onCreate functionality into seperate methods to improve
    code readability.
    
    Added scrollview and some more constraints in main layout to improve
    responsiveness and add scrolling if screen height small.

--- app/src/main/java/com/vinnorman/encryptiondemo/MainActivity.java
+++ app/src/main/java/com/vinnorman/encryptiondemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.vinnorman.encryptiondemo;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -9,6 +10,7 @@ import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
@@ -23,8 +25,8 @@ import java.security.PublicKey;
 public class MainActivity extends AppCompatActivity {
 
 
-    private static final String STATE_TAG_ENCRYPTED_TEXT = "EncryptedText";
-    private static final String STATE_TAG_PRIVATE_KEY = "private key";
+    private static final String STATE_TAG_ENCRYPTED_TEXT = "encrypted_text";
+    private static final String STATE_TAG_PRIVATE_KEY = "private_key";
 
     private PrivateKey privateKey;
     private Button encryptButton, decryptButton;
@@ -36,17 +38,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-        encryptButton = findViewById(R.id.button_encrypt);
-        decryptButton = findViewById(R.id.button_decrypt);
-        decryptedTextView = findViewById(R.id.textview_decrypted);
-        encryptedTextView = findViewById(R.id.textview_encrypted);
-        plainTextInputField = findViewById(R.id.edittext_plain_text_input);
-
         encryptionHelper = new EncryptionHelper();
-
+        setContentView(R.layout.activity_main);
+        setViews();
+        setButtonClickListeners();
+        configureEditTextBehavior();
         if (savedInstanceState != null) {
             String existingEncryptedText = savedInstanceState.getString(STATE_TAG_ENCRYPTED_TEXT);
             if (!TextUtils.isEmpty(existingEncryptedText)) {
@@ -55,10 +51,39 @@ public class MainActivity extends AppCompatActivity {
                 decryptButton.setEnabled(true);
             }
         }
+    }
 
-        setButtonClickListeners();
+    private void setViews() {
+        encryptButton = findViewById(R.id.button_encrypt);
+        decryptButton = findViewById(R.id.button_decrypt);
+        decryptedTextView = findViewById(R.id.textview_decrypted);
+        encryptedTextView = findViewById(R.id.textview_encrypted);
+        plainTextInputField = findViewById(R.id.edittext_plain_text_input);
+    }
 
+    private void setButtonClickListeners() {
+        encryptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performEncryption();
+                plainTextInputField.setText(null);
+                decryptButton.setEnabled(true);
+                plainTextInputField.setCursorVisible(false);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(plainTextInputField.getWindowToken(), 0);
+                }
+            }
+        });
+        decryptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performDecryption();
+            }
+        });
+    }
 
+    private void configureEditTextBehavior() {
         plainTextInputField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -90,37 +115,28 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
     }
-
-    private void setButtonClickListeners() {
-        encryptButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                performEncryption();
-                plainTextInputField.setText(null);
-                decryptButton.setEnabled(true);
-                plainTextInputField.setCursorVisible(false);
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                if (imm != null) {
-                    imm.hideSoftInputFromWindow(plainTextInputField.getWindowToken(), 0);
-                }
-            }
-        });
-        decryptButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                performDecryption();
-            }
-        });
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_about:
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.all_about_this_app)
+                        .setMessage(R.string.about_dialog_content)
+                        .setPositiveButton("Ok", null)
+                        .show();
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
